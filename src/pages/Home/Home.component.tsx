8a1dfd466736d7a01b556d7efe2598d286d7ce2a
Dionescha <dioneluisschallenberger@hotmail.com>
2022-10-08 11:26:57 -0300

    feat: add user service

--- src/pages/Home/Home.component.tsx
+++ src/pages/Home/Home.component.tsx
@@ -4,31 +4,36 @@ import Input from '../../components/atoms/Input'
 import Container from '../../components/atoms/Container'
 import Paper from '../../components/atoms/Paper'
 import Typography from '../../components/atoms/Typography'
+import * as UserService from '../../services/Users'
 
+const Home = () => {
 
-const Home = () => (
-
-  <Container 
-    fullHeight 
-    fullCentered>
-    <Paper>
+  const handleSubmit = (cpf: string) => {
+    UserService.getByCpf(cpf)
+    .then((response) => {
+      console.log(response)
+    }
+  }
+  return (
+    <Container 
+      fullHeight 
+      fullCentered>
+      <Paper>
   
-      <Typography as="h1">Bem Vindo!</Typography>
-      <Input
-        type='text'
-      />
-      <br/>
-      <Button>
+        <Typography as="h1">Bem Vindo!</Typography>
+        <Input
+          type='text'
+        />
+        <br/>
+        <Button
+          onClick={() => handleSubmit('04335816020')}>
         Entrar
-      </Button>
+        </Button>
     
-    </Paper>
-  </Container>
-    
+      </Paper>
+    </Container>
+  )}
+
   
-    
-    
-  
-)
 
 export default Home
--- src/services/Users/index.ts
+++ src/services/Users/index.ts
@@ -1,3 +1,10 @@
 import api from '../../config/api'
+import * as UserService from '../../services/Users'
 
-const getByCpf = (cpf: string) => api
+const path= 'easy-coffee/v1/customer/'
+
+const getByCpf = (cpf: string) => api.get<string>(`${path}get-by-cpf/${cpf}`)
+  .then((response) => response.data)
+export {
+  getByCpf
+}
